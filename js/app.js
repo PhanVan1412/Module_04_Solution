d7151eefc65d7e5bb435f3db8bf98abe1ea8ed5f
Phan Van Tam <phanvantam2020@gmail.com>
2022-05-05 08:52:44 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function solution_01(givenName) {
+    if (givenName[0] == 'j' || givenName[0] == 'J') {
+        console.log('Goodbye ' + 'J' + givenName) 
+    }
+    else {
+        console.log('Hello ' + givenName)
+    }
+}
+solution_01('Phan Van Tam')
+
